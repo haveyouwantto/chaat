932d5fd626cad88100624eb8634d835812c53afc
haveyouwantto <haveyouwantto@gmail.com>
2019-02-09 03:51:33 +0800

    Release 1.0
    Signed-off-by: haveyouwantto <haveyouwantto@gmail.com>

--- chat.js
+++ chat.js
@@ -1,10 +1,17 @@
-var WebSocketServer = require('ws').Server, webSocketServer = new WebSocketServer({ port: 7777 });
+var WebSocketServer = require('ws').Server, webSocketServer = new WebSocketServer({ port: 16575 });
 var HashMap = require('hashmap');
 var readline = require("readline");
+var fs = require("fs");
 var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+var stream = fs.createWriteStream(Date.now()+".log", {flags:'a'});
+function log(msg) {
+	console.log(new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString() + " "+msg)
+    stream.write(new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString() + " "+msg + "\n");
+}
+
 console.log("Server started.")
 // record the client
 var users = new HashMap();
@@ -37,21 +44,24 @@ webSocketServer.on('connection', function (ws) {
         //console.log(connectNum + ": " + message)
         var msg = JSON.parse(message);
         if (msg.msg == "/login") {
-            if (users.get(msg.sender) == null&&users.get(msg.sender) != "Server") {
+            if (users.get(msg.sender) == null && users.get(msg.sender) != "Server") {
                 userid.set(msg.id, ws)
                 users.set(msg.sender, msg.id)
                 sendAll(msg.sender + " 加入服务器")
-                console.log('A client has connected. ID: ' + msg.id);
+                log('A client has connected. ID: ' + msg.id);
             }
             else {
                 send("加入失败，已存在相同名称的用户")
             }
 
-        } else if (msg.msg == "/list") { sendAll(users.keys().toString()) } else {
+        } else if (msg.msg == "/list") {
+            sendAll(msg.msg, msg.sender);
+            sendAll("当前在线：" + users.keys().toString())
+        } else {
             sendAll(msg.msg, msg.sender);
 
         }
-        console.log(new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString() + " <" + msg.sender + "> " + msg.msg);
+        log("<" + msg.sender + "> " + msg.msg);
 
     });
 
@@ -60,11 +70,12 @@ webSocketServer.on('connection', function (ws) {
         var disc2 = users.search(disc)
         userid.remove(disc)
         users.remove(disc2)
-        if(disc!=null){
-        console.log('A client has disconnected. ID: ' + disc);
-        sendAll(disc2 + " 退出服务器")}
+        if (disc != null) {
+            console.log('A client has disconnected. ID: ' + disc);
+            sendAll(disc2 + " 退出服务器")
+        }
     });
-   // rl.on("line", function (line) {
-   //     send(line);
-   // })
+    // rl.on("line", function (line) {
+    //     send(line);
+    // })
 });
